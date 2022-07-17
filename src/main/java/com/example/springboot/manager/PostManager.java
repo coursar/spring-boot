a375b39c3693ffa50333a12f18ffd25af49573a1
student <student@localhost>
2022-07-17 09:38:13 +0300

    post entity fixed

--- src/main/java/com/example/springboot/manager/PostManager.java
+++ src/main/java/com/example/springboot/manager/PostManager.java
@@ -10,6 +10,7 @@ import com.example.springboot.repository.PostRepository;
 import com.example.springboot.security.Authentication;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,6 +18,7 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Component
+@Transactional
 @RequiredArgsConstructor
 public class PostManager {
     private final PostRepository postRepository; // мне через DI подставят нужный интерфейс
@@ -24,8 +26,11 @@ public class PostManager {
             postEntity.getId(),
             postEntity.getContent(),
             postEntity.getTags(),
-            // postEntity.getGeo() != null ? new Geo(postEntity.getGeo().getLat(), postEntity.getGeo().getLng()),
-            Optional.of(postEntity.getGeo()).map(o -> new GeoDTO(o.getLat(), o.getLng())).orElse(null)
+            // postEntity.getGeo() != null ? new GeoDTO(postEntity.getGeo().getLat(), postEntity.getGeo().getLng()) : null
+            // List.of, Map.of, Optional.of - очень не любят null
+            Optional.ofNullable(postEntity.getGeo())
+                    .map(o -> new GeoDTO(o.getLat(), o.getLng()))
+                    .orElse(null)
     );
 
     public List<PostResponseDTO> getAll(final Authentication authentication) {
@@ -48,7 +53,9 @@ public class PostManager {
                 0,
                 requestDTO.getContent(),
                 requestDTO.getTags(),
-                Optional.of(requestDTO.getGeo()).map(o -> new GeoEmbeddable(o.getLat(), o.getLng())).orElse(null)
+                Optional.ofNullable(requestDTO.getGeo())
+                        .map(o -> new GeoEmbeddable(o.getLat(), o.getLng()))
+                        .orElse(null)
         );
         final PostEntity savedEntity = postRepository.save(postEntity);
         return postEntityToPostResponseDTO.apply(savedEntity);
@@ -62,7 +69,9 @@ public class PostManager {
         postEntity.setContent(requestDTO.getContent());
         postEntity.setTags(requestDTO.getTags());
         postEntity.setGeo(
-                Optional.of(requestDTO.getGeo()).map(o -> new GeoEmbeddable(o.getLat(), o.getLng())).orElse(null)
+                Optional.ofNullable(requestDTO.getGeo())
+                        .map(o -> new GeoEmbeddable(o.getLat(), o.getLng()))
+                        .orElse(null)
         );
         return postEntityToPostResponseDTO.apply(postEntity);
     }
